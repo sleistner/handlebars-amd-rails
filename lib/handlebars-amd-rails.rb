17d30f21002a297bdcaba3a9444b65055990f378
Steffen Leistner <sleistner@gmail.com>
2013-02-21 19:25:29 +0100

    Add config.

--- lib/handlebars-amd-rails.rb
+++ lib/handlebars-amd-rails.rb
@@ -1,9 +1,26 @@
-require "handlebars-amd-rails/version"
+require "handlebars-amd-rails/version.rb"
+require "handlebars-amd-rails/rails/engine.rb"
+require "handlebars-amd-rails/rails/railtie.rb"
 
 module Handlebars
-  module Amd
-    module Rails
-      # Your code goes here...
+
+  class Config < OpenStruct
+
+    def to_camelcase
+      inject({}) do |memo, (key, value)|
+        memo[key.to_s.camelcase(:lower)] = value
+        memo
+      end
     end
+
+  end
+
+  def self.config
+    @@config ||= Config.new
+  end
+
+  def self.configure
+    yield self.config
   end
 end
+
