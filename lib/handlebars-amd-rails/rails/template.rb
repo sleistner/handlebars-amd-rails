fa61e70bf68ffe2e87e7d0fa769499f568764201
Steffen Leistner <sleistner@gmail.com>
2013-02-23 22:18:35 +0100

    Use inline interpolation.

--- lib/handlebars-amd-rails/rails/template.rb
+++ lib/handlebars-amd-rails/rails/template.rb
@@ -2,13 +2,20 @@ require 'tilt'
 require 'execjs'
 
 module Handlebars
+
+  module Unindent
+    def unindent(heredoc)
+      heredoc.gsub(/^#{heredoc[/\A\s*/]}/, '')
+    end
+  end
+
   module Amd
     module Rails
 
       module Source
 
         def self.path
-          @path ||= File.expand_path('../../../../vendor/assets/javascripts/handlebars-for-compile.js', __FILE__)
+          @path ||= File.expand_path("../../../../vendor/assets/javascripts/handlebars.js", __FILE__)
         end
 
         def self.contents
@@ -23,6 +30,8 @@ module Handlebars
 
       class Template < ::Tilt::Template
 
+        include Unindent
+
         def self.default_mime_type
           'application/javascript'
         end
@@ -31,8 +40,28 @@ module Handlebars
         end
 
         def evaluate(scope, locals, &block)
-          Source.context.call("Handlebars.compileAmd", data, Handlebars.config.to_camelcase)
+          template = Source.context.call("Handlebars.precompile", data, Handlebars.config.to_camelcase)
+          interpolate(template)
         end
+
+        private
+
+        def interpolate(template)
+          unindent <<-JS
+            define(['handlebars'], function(Handlebars) {
+              var templates = Handlebars.templates || (Handlebars.templates = {});
+              return templates['#{template_name}'] = Handlebars.template(#{template});
+            });
+          JS
+        end
+
+        def template_name
+          @template_name ||= begin
+            template_root = Handlebars.config.template_root
+            file.split(template_root).last.split('.', 2).first
+          end
+        end
+
       end
     end
   end
