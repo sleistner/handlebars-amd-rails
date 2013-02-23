eb54cee33c0b7809f282bea1f5bb1a53bf1e723b
Steffen Leistner <sleistner@gmail.com>
2013-02-23 22:19:05 +0100

    Rename initializer.

--- lib/handlebars-amd-rails/rails/engine.rb
+++ lib/handlebars-amd-rails/rails/engine.rb
@@ -1,11 +1,10 @@
 require "rails/engine"
-require "template.rb"
 
 module Handlebars
   module Amd
     module Rails
       class Engine < ::Rails::Engine
-        initializer :register_dustjs do |app|
+        initializer :register_handlebars do |app|
           app.assets.register_engine '.hbs', Template
           app.assets.register_engine '.handlebars', Template
         end
